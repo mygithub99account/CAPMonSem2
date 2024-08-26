17327d64d1728c908e7dc298aa8948b62e89113a
Izzy <20972461@students.latrobe.edu.au>
2024-08-26 15:56:05 +1000

    I made some cool changes!

---
+++ CheckThisOut.py
@@ -0,0 +1,10 @@
+print("This is why Java is the worst and Python reigns supreme!!")
+x = 5
+print("HI THERE \n" * x)
+
+
+print("Mamaaaaaaa Ohhhhhhh\n")
+print("Didn't mean to make you cry\n")
+print("If I'm not back again this time tomorrow\n")
+print("Carry on, carry on as if nothing really matters\n")
+print(" - Freddie Mercury, Queen")
--- HelloCap.java
+++ HelloCap.java
@@ -2,6 +2,6 @@ public class HelloCap {
 
     public static void main(String[] args) {
         System.out.println("Hello Capstone");
-    }
+        System.out.println("I will pretend, just for today, that Java is the best language.");}
     
 }
